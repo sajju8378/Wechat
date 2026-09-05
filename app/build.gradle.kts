0b763c6d230c4132a40566411591914733e9ad47
sajju8378 <sajju8378@gmail.com>
2026-09-05 12:24:15 +0530

    Configure Wechat Android app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,22 @@
 plugins {
-    id("com.android.application") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.sajju.wechat"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.sajju.wechat"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
 }
